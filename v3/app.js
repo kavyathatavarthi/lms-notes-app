64444c530249562e230bc0ae8e8ef250fb21b685
kavyathatavarthi <kavyathatavarthi@email.com>
2026-04-05 22:50:08 +0530

    modified

--- v3/app.js
+++ v3/app.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => console.log("LMS v1 running"));
+app.listen(3000, () => console.log("LMS v3 running"));
